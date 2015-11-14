c377ad876373c2fdedb73b742b2c4996e6998a75
Mikhail Swift <mikhail.swift@gmail.com>
2015-11-14 04:43:28 -0500

    tinkering and playing with unique ptrs, etc

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,10 @@
 //
 
 #include <SDL2/SDL.h>
+#include <memory>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-
-namespace GameEngine
-{
-    void HandleError();
-}
-
-int main()
-{
-    SDL_Window *window = NULL;
-    SDL_Surface *surface = NULL;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        GameEngine::HandleError();
-        return 0;
-    }
-
-    window = SDL_CreateWindow("tits", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL)
-    {
-        GameEngine::HandleError();
-        return 0;
-    }
-
-    surface = SDL_GetWindowSurface(window);
-    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(2000);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
-}
 
 namespace GameEngine
 {
@@ -44,4 +14,35 @@ namespace GameEngine
     {
         printf("SHIT! %s", SDL_GetError());
     }
+
+    struct SDL_Deleter
+    {
+        void operator()(SDL_Window *window) const { SDL_DestroyWindow(window); }
+    };
+}
+
+int main()
+{
+    std::unique_ptr<SDL_Window, GameEngine::SDL_Deleter> window;
+    SDL_Surface *surface = NULL;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        GameEngine::HandleError();
+        return 0;
+    }
+
+    window = std::unique_ptr<SDL_Window, GameEngine::SDL_Deleter>
+            (SDL_CreateWindow("tits", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN));
+    if (window == NULL)
+    {
+        GameEngine::HandleError();
+        return 0;
+    }
+
+    surface = SDL_GetWindowSurface(window.get());
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window.get());
+    SDL_Delay(2000);
+    SDL_Quit();
+    return 0;
 }
